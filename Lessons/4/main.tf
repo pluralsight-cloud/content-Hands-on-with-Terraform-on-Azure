69599a78a4a06e9bfbda8a89a6213bfd6c6c2910
Wayne Hoggett <wayne-hoggett@pluralsight.com>
2023-02-16 14:37:04 +1000

    Updates

---
+++ Lessons/4/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+  
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "" {
+  name     = ""
+  location = ""
+}
