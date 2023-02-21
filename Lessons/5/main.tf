ffe2dcbe70d54771373c089365ddf76f9162eb25
Wayne Hoggett <wayne-hoggett@pluralsight.com>
2023-02-21 10:16:28 +1000

    Various Updates

---
+++ Lessons/5/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "~> 1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version  = ">=3.43.0"
+    }
+  }
+  #Terraform cloud code block
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = ""
+  location = ""
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "rcaistg434534"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
